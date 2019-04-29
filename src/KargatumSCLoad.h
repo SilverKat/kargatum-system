c4c2b4368c0e16434a41cd0316de39c056e3ceab
Winfidonarleyan <dowlandtop@yandex.ru>
2019-04-29 18:41:23 +0700

    Cmake: Only CFBG

--- src/KargatumSCLoad.h
+++ src/KargatumSCLoad.h
@@ -12,17 +12,6 @@ void AddSC_Kargatum_Startup();
 #endif
 
 // From SC
-void AddSC_Kargatum_AntiAdvertisment();
-void AddSC_Kargatum_Auto_Revive();
-void AddSC_Kargatum_BGReward();
-void AddSC_Kargatum_BuffCommand();
-void AddSC_Kargatum_IconFaction();
-void AddSC_Kargatum_GMChatColor();
-void AddSC_Kargatum_LevelReward();
-void AddSC_Kargatum_PlayerNotSpeak();
-void AddSC_Kargatum_OnlineReward();
-void AddSC_Kargatum_PlayerLoginInfo();
-void AddSC_Kargatum_SpellDuration();
 void AddSC_Kargatum_CFBG();
 
 // Add all
@@ -32,19 +21,6 @@ void AddKargatumScripts()
     // Lib
     AddSC_Kargatum_Startup();
 #endif
-
-    // SC
-    AddSC_Kargatum_AntiAdvertisment();
-    AddSC_Kargatum_Auto_Revive();
-    AddSC_Kargatum_BGReward();
-    AddSC_Kargatum_BuffCommand();
-    AddSC_Kargatum_IconFaction();
-    AddSC_Kargatum_GMChatColor();
-    AddSC_Kargatum_LevelReward();
-    AddSC_Kargatum_PlayerNotSpeak();
-    AddSC_Kargatum_OnlineReward();
-    AddSC_Kargatum_PlayerLoginInfo();
-    AddSC_Kargatum_SpellDuration();
 
 #ifdef KARGATUM_CFBG
     AddSC_Kargatum_CFBG();
